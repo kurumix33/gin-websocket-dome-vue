267bd116b1ac9c52c5b7a4bebbeea4686f0008cf
sdttttt <760159537@qq.com>
2019-09-20 21:45:45 +0800

    优化细节，做了一部分的权限控制和很Happt的东西

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-09-20 15:54:23
- * @LastEditTime: 2019-09-20 19:04:34
+ * @LastEditTime: 2019-09-20 20:27:39
  * @LastEditors: Please set LastEditors
  */
 import Vue from 'vue'
@@ -11,6 +11,7 @@ import HelloWorld from '@/components/HelloWorld'
 import TestOne from '@/components/TestOne'
 import Login　from "@/components/Login"
 import NotFound from "@/components/error/NotFound"
+import Happy from '@/components/Happy'
 
 Vue.use(Router)
 
@@ -21,6 +22,14 @@ export default new Router({
       //组件名
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/happy',
+      name: 'Happy',
+      component: Happy,
+      meta: {
+        needLogin: true
+      }
     },
     {
       path: '/fuck',
